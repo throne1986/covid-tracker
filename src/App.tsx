e8ea25cee90390606613e055ac141c1ea9bc32c3
gzellah <jellyforjully088@yahoo.com>
2022-08-08 12:12:52 +0200

    Added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from "./components/Home"
 
@@ -6,6 +7,7 @@ export default function App() {
     <>
     <Header />
     <Home />
+    <Footer />
     </>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { BottomNavigation, Box, Typography } from '@mui/material'
+
+const  Footer: React.FC = () => {
+  return (
+    <Box id="main-footer">
+        <BottomNavigation showLabels className='foote-info'>
+            <Typography color="textSecondary" align="center">
+                Copyright Resevered © 2022 Zellah's Inc, Created by Geofrey Zellah
+            </Typography>
+        </BottomNavigation>
+    </Box>
+  )
+}
+
+export default Footer
